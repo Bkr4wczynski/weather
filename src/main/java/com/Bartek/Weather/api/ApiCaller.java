53ce59a383edc0d08a0c09ba5aa305ebed075823
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-09-26 18:17:12 +0200

    APP FINISHED

--- src/main/java/com/Bartek/Weather/api/ApiCaller.java
+++ src/main/java/com/Bartek/Weather/api/ApiCaller.java
@@ -3,6 +3,8 @@ package com.Bartek.Weather.api;
 import org.springframework.web.client.RestClient;
 
 import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class ApiCaller {
@@ -11,7 +13,7 @@ public class ApiCaller {
 
     private URI createUri(String city, String unitGroup) {
         return URI.create("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
-                + city + "/last1days?unitGroup=" + unitGroup + "&key=" + API_KEY + "&contentType=json");
+                + URLEncoder.encode(city, StandardCharsets.UTF_8) + "/last1days?unitGroup=" + unitGroup + "&key=" + API_KEY + "&contentType=json");
     }
 
     public Map getWeather(String city, String unitGroup) {
--- src/main/java/com/Bartek/Weather/controller/MainController.java
+++ src/main/java/com/Bartek/Weather/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.*;
 
@@ -22,16 +23,22 @@ public class MainController {
 
     @PostMapping("/getWeather")
     public String getWeather(@RequestParam String city, @RequestParam String unitGroup, Model model) {
-        // Map weather = apiCaller.getWeather(city, unitGroup);
-        // List<String> formattedWeather = formatWeatherData(weather);
-        List<String> formattedWeather = Arrays.asList("23.0", "New York", "10.0", "50.0");
-        model.addAttribute("weather", formattedWeather);
+        try{
+            Map weather = apiCaller.getWeather(city, unitGroup);
+            List<String> formattedWeather = formatWeatherData(weather);
+            model.addAttribute("weather", formattedWeather);
+        }
+        catch (HttpClientErrorException e) {
+            model.addAttribute("errorMessage", "City has not been found!");
+        }
         return "main.html";
+
+        //List<String> formattedWeather = Arrays.asList("23.0", "New York", "50.0", "10.0");
     }
 
     private List<String> formatWeatherData(Map weatherData) {
         List<String> formattedWeatherData = new ArrayList<>();
-        LinkedHashMap dayWeatherData = (LinkedHashMap) ((ArrayList<?>) weatherData.get("days")).get(0);
+        LinkedHashMap dayWeatherData = (LinkedHashMap) ((ArrayList<?>) weatherData.get("days")).getFirst();
 
         formattedWeatherData.add(String.valueOf(dayWeatherData.get("temp")));
         formattedWeatherData.add(String.valueOf(weatherData.get("resolvedAddress")));
